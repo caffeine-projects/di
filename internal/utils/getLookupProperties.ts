31fd5a40191f3b302b34fd1a73683dca95f85ebc
Vitor Hugo Salgado <2922948+vitorsalgado@users.noreply.github.com>
2024-04-25 23:13:05 +0200

    ci: eslint + prettier + github actions

--- internal/utils/getLookupProperties.ts
+++ internal/utils/getLookupProperties.ts
@@ -1,7 +1,9 @@
 import { Vars } from '../Vars.js'
 import { TokenSpec } from '../../Token.js'
 
-export function getLookupProperties<TFunction extends Object>(target: TFunction): [string | symbol, TokenSpec<unknown>][] {
+export function getLookupProperties<TFunction extends Object>(
+  target: TFunction,
+): [string | symbol, TokenSpec<unknown>][] {
   const injectionTokens: Record<string, TokenSpec<unknown>> = Reflect.getOwnMetadata(
     Vars.CLASS_LOOKUP_PROPERTIES,
     target,
