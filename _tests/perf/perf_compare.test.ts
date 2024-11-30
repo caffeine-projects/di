c05bf0717555770086fd62d3f7433d9e23b09295
Vitor Hugo Salgado <2922948+vitorsalgado@users.noreply.github.com>
2024-11-30 22:17:57 +0100

    perf: better results

--- _tests/perf/perf_compare.test.ts
+++ _tests/perf/perf_compare.test.ts
@@ -5,7 +5,6 @@ import { performance } from 'node:perf_hooks'
 import { describe, it } from 'node:test'
 import { expect } from 'expect'
 import { green, yellow } from 'colorette'
-import { gray } from 'colorette'
 import { Root } from './_testdata/di.js'
 import { di } from './_testdata/di.js'
 import { RootSingleton } from './_testdata/di.js'
@@ -17,13 +16,14 @@ import { bootstrap, NestTransientRoot } from './_testdata/third_party_legacy/dis
 import { NestRoot } from './_testdata/third_party_legacy/dist/nest.js'
 import { loopCtx, LoopSingletonRoot } from './_testdata/third_party_legacy/dist/loopback.js'
 import { LoopRoot } from './_testdata/third_party_legacy/dist/loopback.js'
+import { table } from 'table'
 
 const failIfLess = process.env.TEST_FAIL_PERF === 'true'
 const iterations = 15000
 const items = new Array<{ pos: number; total: number }>(iterations)
-const diff = (a: number, b: number) => '~' + String(Math.round(((a - b) / b) * 100)) + '%'
+const diff = (a: number, b: number): number => Math.round(((a - b) / b) * 100)
 
-describe('Performance Compare', function () {
+describe('Performance Comparison', function () {
   interface Results {
     avg: number
     max: number
@@ -52,6 +52,7 @@ describe('Performance Compare', function () {
       if (total < result.min) {
         result.min = total
       }
+
       if (total > result.max) {
         result.max = total
       }
@@ -100,8 +101,14 @@ describe('Performance Compare', function () {
   }
 
   it(`should resolve ${iterations} times in less time then the others`, async () => {
+    const results: any[][] = [['Lib', 'Avg', 'Diff']]
+    const resultsSingleton: any[][] = [['Lib', 'Avg', 'Diff']]
+    const resultsAsync: any[][] = [['Lib', 'Avg', 'Diff']]
+
+    // setup
     const nestApp = await bootstrap()
 
+    // ensuring everything was correctly configured
     expect(inv.get(InvRoot)).toBeInstanceOf(InvRoot)
     expect(inv.get(InvSingletonRoot)).toBeInstanceOf(InvSingletonRoot)
     expect(tsy.resolve(TsyRoot)).toBeInstanceOf(TsyRoot)
@@ -120,32 +127,57 @@ describe('Performance Compare', function () {
     const loopRes = resolve(iterations, () => loopCtx.getSync(LoopRoot.name))
     const loopSingletonRes = resolve(iterations, () => loopCtx.getSync(LoopSingletonRoot.name))
 
-    const diAsync = await resolveAsync(iterations, () => di.getAsync(RootSingleton))
+    // async runs
+    const diAsync = await resolveAsync(iterations, () => di.getAsync(Root))
+    const diAsyncSingleton = await resolveAsync(iterations, () => di.getAsync(RootSingleton))
 
-    console.log('DI Avg: ' + gray(String(diRes.avg)))
-    console.log('DI Singleton Avg: ' + gray(String(diSingletonRes.avg)))
-    console.log('DI Async Avg: ' + gray(String(diAsync.avg)))
+    // printing results
+    results.push(['DI', diRes.avg, '-'])
+    resultsSingleton.push(['DI Singleton', diSingletonRes.avg, '-'])
+    resultsAsync.push(['DI Async', diAsync.avg, '-'])
+    resultsAsync.push(['DI Async Singleton', diAsyncSingleton.avg, '-'])
 
-    function check(name: string, di: Results, compareTo: Results) {
-      const msg = `${name}: diff: ${diff(di.avg, compareTo.avg)}`
+    function addResult(name: string, arr: any[][], base: Results, target: Results) {
+      let df = String(diff(base.avg, target.avg)) + '%'
 
-      console.log(`${name}: avg: ${gray(String(compareTo.avg))}`)
-
-      if (di.avg > compareTo.avg) {
-        console.log(yellow(msg))
+      if (base.avg > target.avg) {
+        df = yellow(df)
       } else {
-        console.log(green(msg))
+        df = green(df)
       }
+
+      arr.push([name, target.avg, df])
     }
 
-    check('inversify', diRes, invRes)
-    check('inversify singleton', diSingletonRes, invSingletonRes)
-    check('tsyringe', diRes, tsyRes)
-    check('tsyringe singleton', diSingletonRes, tsySingletonRes)
-    check('loopback', diRes, loopRes)
-    check('loopback singleton', diSingletonRes, loopSingletonRes)
-    check('nestjs', diRes, nestRes)
-    check('nestjs singleton', diSingletonRes, nestSingletonRes)
+    addResult('Inversify', results, diRes, invRes)
+    addResult('Inversify Singleton', resultsSingleton, diSingletonRes, invSingletonRes)
+    addResult('Tsyringe', results, diRes, tsyRes)
+    addResult('Tsyringe Singleton', resultsSingleton, diSingletonRes, tsySingletonRes)
+    addResult('Loopback', results, diRes, loopRes)
+    addResult('Loopback Singleton', resultsSingleton, diSingletonRes, loopSingletonRes)
+    addResult('Nestjs', resultsAsync, diAsync, nestRes)
+    addResult('Nestjs Singleton', resultsAsync, diAsyncSingleton, nestSingletonRes)
+
+    console.log(
+      table(
+        results.sort((a, b) => a[1] - b[1]),
+        { header: { content: 'Perf' } },
+      ),
+    )
+
+    console.log(
+      table(
+        resultsSingleton.sort((a, b) => a[1] - b[1]),
+        { header: { content: 'Perf - Singleton' } },
+      ),
+    )
+
+    console.log(
+      table(
+        resultsAsync.sort((a, b) => a[1] - b[1]),
+        { header: { content: 'Perf - Async' } },
+      ),
+    )
 
     if (failIfLess) {
       expect(diRes.avg).toBeLessThan(invRes.avg)
